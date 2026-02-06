f7ee5ca709bd34a53a3800909d12f3514921628d
Manansh123 <148788715+Manansh123@users.noreply.github.com>
2026-02-06 10:07:22 +0530

    Time: 4 ms (99.79%) | Memory: 51.3 MB (54.17%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -1,10 +1,10 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k<=1) return 0;
+        // if (k<=1) return 0;  // not use u can only check left<=right because for testcase 2 left aage badhta jaaayega right ke bhi aage
         int start=0, end=0, count=0, pro=1;
         while (end<nums.length) {
             pro*=nums[end];
-            while (pro>=k) {
+            while (start<=end && pro>=k) {
                 pro/=nums[start];
                 start++;
             }
