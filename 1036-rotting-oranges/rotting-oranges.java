d7da3a5d1004e13508d61766b669eb1db6c9a05c
Manansh123 <148788715+Manansh123@users.noreply.github.com>
2026-02-15 00:00:51 +0530

    Time: 2 ms (90.81%) | Memory: 44.2 MB (55.81%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,56 +1,53 @@
+//level wise level order traversal
 class Solution {
-    class pair {
+    class pair{
         int row;
         int col;
         public pair(int row, int col) {
-            this.row = row;
-            this.col = col;
+            this.row=row;
+            this.col=col;
         }
     }
-    int[] rowdirection = { -1, 0, 0, 1 };
-    int[] coldirection = { 0, 1, -1, 0 };
+    int[] rowdirection={-1, 0, 0, 1};
+    int[] coldirection={0, 1, -1, 0};
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int min = -1;
-        boolean[][] visited = new boolean[n][m];
-        Queue<pair> q = new LinkedList<>();
-        int count = 0; //count fresh
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) { // rotten tomatoes
+        int n=grid.length;
+        int m=grid[0].length;
+        Queue<pair> q=new LinkedList<>();
+        boolean[][] visited=new boolean[n][m];
+        int onecount=0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (grid[i][j]==2 && !visited[i][j]) {
                     q.add(new pair(i, j));
-                    visited[i][j] = true;
                 }
-                if( grid[i][j] == 1) count++;
+                if (grid[i][j]==1) onecount++;
             }
         }
-        if (count==0) return 0;
-        if (q.isEmpty())
-            return -1;
-        // apply level wise level order traversal
+        int minute=0;
         while (!q.isEmpty()) {
-            int size = q.size();
-            // boolean flag=false; // to check that ki agar ek level pe koi rott hua hai yaa nhi
-            while(size > 0) {
-                pair curr = q.poll();
-                for (int i = 0; i < 4; i++) {
-                    int neighrow = curr.row + rowdirection[i];
-                    int neighcol = curr.col + coldirection[i];
-                    if (isvalid(neighrow, neighcol, n, m) && grid[neighrow][neighcol] == 1 && !visited[neighrow][neighcol]) {
+            int size=q.size();
+            boolean flag=false; // this will check agar kooi fresh orrange ott hua hai tbhi minute++ kro
+            while(size>0) {
+                pair curr=q.poll();
+                size--;
+                for (int i=0; i<4; i++) {
+                    int neighrow=curr.row+rowdirection[i];
+                    int neighcol=curr.col+coldirection[i];
+                    if (isvalid(neighrow, neighcol, n, m) && grid[neighrow][neighcol]==1 && !visited[neighrow][neighcol]) {
                         q.add(new pair(neighrow, neighcol));
                         visited[neighrow][neighcol]=true;
-                        count--;
+                        flag=true; //yahan pe fresh se rotten bana toh true krdiya
+                        onecount--;
                     }
                 }
-                size--;
             }
-           min++;
+            if (flag) minute++; //har ek bfs ke level pe hoga
         }
-        if( count > 0 ) return -1;
-        return min;
+        if(onecount>0 && q.isEmpty()) return -1;
+        return minute;
     }
     public boolean isvalid(int row, int col, int n, int m) {
-        return (row>=0 && row<n && col>=0 && col<m) ? true : false;
+        return (row>=0 && row<n && col>=0 && col<m);
     }
 }
