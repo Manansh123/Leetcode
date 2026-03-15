2408d7c442a4927a08b6a37739e2521358ccb8f3
Manansh123 <148788715+Manansh123@users.noreply.github.com>
2026-03-15 17:37:05 +0530

    Time: 11 ms (69.51%) | Memory: 50.1 MB (72.12%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,30 +1,33 @@
 class Solution {
-    private boolean canShip(int[] weights, int days, int mid){
-        int d=1, load=0;
-        for (int i : weights) {
-            if (load+i>mid) {
-                d++;
-                load=0;
-            }
-            load=load+i;
-        }
-        return d <= days;
-    }
     public int shipWithinDays(int[] weights, int days) {
-        int start=0, end=0; 
+        int start=0, end=0, ans=0;
         for (int i : weights) {
             start=Math.max(start, i);
             end+=i;
         }
-        while (start<=end) { 
-            int mid= start+(end-start)/2;
-            if (canShip(weights, days, mid)) {
-                end = mid-1;
-            } 
+        while (start<=end) {
+            int mid=start+(end-start)/2;
+            if (check(weights, mid, days)) {
+                ans=mid;
+                end=mid-1;
+            }
             else {
-                start = mid + 1;
+                start=mid+1;
             }
         }
-        return start;
+        return ans;
+    }
+    public boolean check(int[] weights, int mid, int days) {
+        int load=0, d=1;
+        for (int i : weights) {
+            if (load+i>mid) {
+                d++;
+                load=i;
+            }
+            else {
+                load+=i;
+            }
+        }
+        return d<=days;
     }
 }
