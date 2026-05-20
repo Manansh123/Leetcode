75d7ab150dc862b6d3af88782cdc326e518bb83e
Manansh123 <148788715+Manansh123@users.noreply.github.com>
2026-05-20 22:41:09 +0530

    Time: 8 ms (31.32%) | Memory: 47.6 MB (10.71%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -3,18 +3,18 @@ class Solution {
         int n=matrix.length;
         int[][] dp=new int[n][n];
         for (int i=0; i<n; i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
+        for (int i=0; i<n; i++) dp[0][i]=matrix[0][i];
+        for (int i=1; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                int diagright=Integer.MAX_VALUE, diagleft=Integer.MAX_VALUE;
+                if (j>0) diagleft=dp[i-1][j-1];
+                int down=dp[i-1][j];
+                if (j<n-1) diagright=dp[i-1][j+1];
+                dp[i][j]=matrix[i][j]+Math.min(Math.min(diagleft, diagright), down);
+            }
+        }
         int min=Integer.MAX_VALUE;
-        for (int x=0; x<n; x++) min=Math.min(min, solve(0, x, n, matrix, dp));
+        for (int x=0; x<n; x++) min=Math.min(min, dp[n-1][x]);
         return min;
-    }
-    public int solve(int i, int j, int n, int[][] matrix, int[][] dp) {
-        if (j<0 || j>=n) return Integer.MAX_VALUE;
-        if (i==n-1) return matrix[i][j];
-        if (dp[i][j]!=Integer.MAX_VALUE) return dp[i][j];
-        int diagleft=solve(i+1, j-1, n, matrix, dp);
-        int down=solve(i+1, j, n, matrix, dp);
-        int diagright=solve(i+1, j+1, n, matrix, dp);
-        dp[i][j]=matrix[i][j]+Math.min(Math.min(diagleft, diagright), down);
-        return dp[i][j];
     }
 }
