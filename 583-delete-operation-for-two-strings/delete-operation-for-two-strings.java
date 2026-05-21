645c63c44ed4046b8ea48948c0dac4035965eee7
Manansh123 <148788715+Manansh123@users.noreply.github.com>
2026-05-21 17:25:21 +0530

    Time: 7 ms (69.06%) | Memory: 46.5 MB (29.11%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -2,15 +2,15 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
-        int[][] dp=new int[n][m];
-        for (int i=0; i<n; i++) Arrays.fill(dp[i], -1);
-        int lcs=solve(0, 0, n, m, word1, word2, dp);
+        int[][] dp=new int[n+1][m+1];
+        int lcs=0;
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                if (word1.charAt(i-1)==word2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=Math.max(dp[i][j-1], dp[i-1][j]);
+                lcs=dp[i][j];
+            }
+        }
         return (n-lcs)+(m-lcs);
-    }
-    public int solve(int i, int j, int n, int m, String word1, String word2, int[][] dp) {
-        if (i==n || j==m) return 0;
-        if (dp[i][j]!=-1) return dp[i][j];
-        if (word1.charAt(i)==word2.charAt(j)) return dp[i][j]=1+solve(i+1, j+1, n, m, word1, word2, dp);
-        else return dp[i][j]=Math.max(solve(i+1, j, n, m, word1, word2, dp), solve(i, j+1, n, m, word1, word2, dp));
     }
 }
