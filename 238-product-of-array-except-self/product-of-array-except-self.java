902985527801082dad114f5da2d5117f14628e07
Manansh Gupta <148788715+Manansh123@users.noreply.github.com>
2026-09-16 22:38:25 +0530

    Time: 2 ms (92.97%) | Memory: 65 MB (77.73%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,32 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] arr = new int[nums.length];
-        int z = 0, pro = 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                z++;
+        int n=nums.length;
+        int[] pre=new int[n];
+        pre[0]=1;
+        int[] suf=new int[n];
+        suf[n-1]=1;
+        for (int i=1; i<n; i++) {
+            pre[i]=pre[i-1]*nums[i-1];
         }
-        if (z > 1)
-            return arr;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                pro *= nums[i];
-            }
+        for (int i=n-2; i>=0; i--) {
+            suf[i]=suf[i+1]*nums[i+1];
         }
-        if (z == 1) {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 0) {
-                    arr[i] = pro;
-                } else {
-                    arr[i] = 0;
-                }
-            }
-        } else {
-            for (int i = 0; i < nums.length; i++) {
-                arr[i] = pro / nums[i];
-            }
+        int[] res=new int[n];
+        for (int i=0; i<n; i++) {
+            res[i]=pre[i]*suf[i];
         }
-
-        return arr;
+        return res;
     }
 }
